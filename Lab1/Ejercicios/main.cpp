f3306c3c4f8735e956e37ae1e7dd4c976f4a8ad7
juancamilo122006 <juanc.ramirezl@udea.edu.co>
2024-08-26 14:27:56 -0500

    Ejercicio 1 hecho

--- Lab1/Ejercicios/main.cpp
+++ Lab1/Ejercicios/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    cout<<"Ingrese dos numeros";
+    int a,b;
+    cin>> a;
+    cin>> b;
+    int c= a%b;
+    cout<< c,;
     return 0;
 }
+
